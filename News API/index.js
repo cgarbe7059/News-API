10ba822128846f4dcb3cdf03257a9f05de9a6e84
cgarbe7059 <102381559+cgarbe7059@users.noreply.github.com>
2023-03-20 21:36:58 -0400

    Full upload
    Functioning html, css, and js to add to portfolio homepage

---
+++ News API/index.js
@@ -0,0 +1,44 @@
+const searchForm = document.querySelector('.search');
+const input = document.querySelector('.input');
+const newsList = document.querySelector('.news-list');
+
+searchForm.addEventListener('submit', retrieve)
+
+function retrieve(e){
+
+    if(input.value == ''){
+        alert('Input field is empty')
+        return
+    }
+
+    newsList.innerHTML = ''
+
+    e.preventDefault()
+
+    const apiKey = "d81f904a62c24564bed95c4ad63a0309"
+    let topic = input.value;
+    
+    let url = `https://newsapi.org/v2/everything?q=${topic}&apiKey=${apiKey}`
+
+    fetch(url).then((res)=>{
+        return res.json()
+    }).then((data)=>{
+        console.log(data)
+        data.articles.forEach(article => {
+            let li = document.createElement('li');
+            let a = document.createElement('a');
+            a.setAttribute('href', article.url );
+            a.setAttribute('target', '_blank')
+            a.textContent = article.title;
+            li.appendChild(a);
+            newsList.appendChild(li);
+        })
+
+    }).catch((error)=>{
+        console.log(error)
+
+
+    })
+    
+
+}
